b8bc43f4c2ce2a1b3f2d0a5531b144b435bf15ab
Ahmad Alhashemi <ahmad@ahmadh.com>
2020-06-11 01:57:52 +0300

    Initial Commit

---
+++ Farewell/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  Farewell
+//
+//  Created by Ahmad Alhashemi on 2020-06-11.
+//  Copyright © 2020 Ahmad Alhashemi. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
